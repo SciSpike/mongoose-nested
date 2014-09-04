dddcd818a1eddcf902732c8fbf24edff2c50a293
JK <jonathan.kamke@gmail.com>
2014-09-04 11:01:22 -0700

    initial version

---
+++ index.js
@@ -0,0 +1,67 @@
+var mongoose = require("mongoose");
+var SchemaTypes = mongoose.SchemaTypes;
+var Schema = mongoose.Schema;
+function Nested (path, options) {
+  // console.log("Nested",arguments)
+  var required = options.required;
+  var type = options.type;
+  delete options.required;
+  delete options.type;
+  var topPath = path;
+  SchemaTypes.Mixed.call(this, path, options);
+  function validateChildren (arg, cb) {
+    // console.log("validateChildren",arguments)
+    if (!required && !arg) {
+      return cb(true);
+    } else if (require && !arg) {
+      return cb(false);
+    }
+    // console.log("validateChildren",!required, !arg,arguments)
+    var self = this;
+    var schema = new Schema(options, {
+      _id : false
+    });
+
+    var validating = {}, total = 0;
+
+    schema.eachPath(validatePath);
+    var errors = [];
+    function validatePath (path) {
+      if (validating[path])
+        return;
+      // console.log("validatePath",arguments)
+
+      validating[path] = true;
+      total++;
+
+      process.nextTick(function () {
+        // console.log("valud",val)
+        var p = schema.path(path);
+        if (!p)
+          return --total || complete();
+        var cPath = [ topPath, path ].join('.');
+        var val = self.getValue(cPath);
+        // console.log("pre-doValidate",self,val,path)
+        p.doValidate(val, function (err) {
+          // console.log("doValidate",arguments)
+          if (err) {
+            errors.push(err);
+            self.invalidate(cPath, err, undefined, true // embedded docs
+            );
+          }
+          if(--total==0){
+            complete();
+          }
+        }, self);
+      });
+    }
+
+    function complete () {
+      cb(!errors.length);
+    }
+  }
+  this.validate(validateChildren);
+}
+Nested.prototype.__proto__ = SchemaTypes.Mixed.prototype;
+SchemaTypes.Nested = Nested;
+mongoose.Types.Nested = Nested;// Mixed;
