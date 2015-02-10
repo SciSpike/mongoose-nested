8a0daabe3238d0f30db5913d0c4b7d43588295d3
Matthew Adams <matthew@matthewadams.me>
2015-02-10 08:29:08 -0600

    commented out console.log statements

--- index.js
+++ index.js
@@ -2,54 +2,61 @@ var mongoose = require("mongoose");
 var SchemaTypes = mongoose.SchemaTypes;
 var Schema = mongoose.Schema;
 function Nested (path, options) {
-  // console.log("Nested",arguments)
+  //console.log("Nested",arguments)
   var required = options.required;
   var type = options.type;
   delete options.required;
   delete options.type;
   var topPath = path;
+  //console.log('topPath:', path);
+
   SchemaTypes.Mixed.call(this, path, options);
   function validateChildren (arg, cb) {
-    // console.log("validateChildren",arguments)
+    //console.log("validateChildren: arguments: ",JSON.stringify(arguments, undefined, 2));
+    //console.log("validateChildren: this:", JSON.stringify(this,null,2));
     if (!required && !arg) {
       return cb(true);
     } else if (require && !arg) {
       return cb(false);
     }
-    // console.log("validateChildren",!required, !arg,arguments)
-    var self = this;
+    var self = this; // 'this' can be the wrong 'this' & can cause validation errors!
     var schema = new Schema(options, {
       _id : false
     });
 
     var validating = {}, total = 0;
-
-    schema.eachPath(validatePath);
     var errors = [];
+
     function validatePath (path) {
       if (validating[path])
         return;
-      // console.log("validatePath",arguments)
+      //console.log("validatePath called with arguments: ",JSON.stringify(arguments, undefined, 2));
+      //console.log("validating status: ", JSON.stringify(validating, null, 2));
 
       validating[path] = true;
       total++;
 
-      process.nextTick(function () {
-        // console.log("valud",val)
+      //process.nextTick(function () {
+        //console.log("nextTick function called with arguments: ",JSON.stringify(arguments, undefined, 2));
         var p = schema.path(path);
         if (!p)
           return --total || complete();
+        //console.log("topPath is ", topPath);
         var cPath = [ topPath, path ].join('.');
         var val = self.getValue(cPath);
-        // console.log("pre-doValidate",self,val,path)
         if (p.validators.length > 0) {
+          // WEIRD:  adding thiz:this to the stringified object below fixes the problem!!!
+          // console.log('!!!!! IGNORE THE FOLLOWING:  TypeError: Converting circular structure to JSON');
+          // JSON.stringify({thiz:this},null,2);
+          //console.log("validating: ", JSON.stringify({cPath:cPath,val_shown_in_array:[val],p:p,self:self,thiz:this},null,2));
           p.doValidate(val, function (err) {
-            // console.log("doValidate",arguments)
             if (err) {
+              //console.log("validation FAILED: ", JSON.stringify({cPath:cPath,val_shown_in_array:[val],p:p,self:self},null,2));
               errors.push(err);
               self.invalidate(cPath, err, undefined, true // embedded docs
               );
             }
+            //console.log("validation OK: ", JSON.stringify({cPath:cPath,val:val,p:p},null,2));
             if(--total==0){
               complete();
             }
@@ -59,12 +66,15 @@ function Nested (path, options) {
             complete();
           }
         }
-      });
+      //});
     }
 
     function complete () {
       cb(!errors.length);
     }
+
+    schema.eachPath(validatePath);
+
   }
   this.validate(validateChildren);
 }
